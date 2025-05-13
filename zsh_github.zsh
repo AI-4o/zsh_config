c95d4839639b161186d4dbdd8c75befdcb69319a
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-05-13 11:28:06 +0200

    rename gh utilities

--- zsh_github.zsh
+++ zsh_github.zsh
@@ -1,28 +1,38 @@
 # GITHUB
 
-function grpc() {
+function ghc() {
   if [[ -z "$1" ]]; then
     echo "Error: repository name is required."
     echo "Use '-h' for help."
     return 1
   fi
   if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "Usage: gcrepo <repository name>"
+    echo "Create a new GitHub repository and add it as the origin remote."
+    echo "Usage: ghc [option]"
+    echo "option can be:"
+    echo "  -h, --help: Show this help message"
+    echo "  -p: Create a public repository"
+    echo "no option: Create a private repository"
+
     echo "Creates a new repository on GitHub and adds it as the origin remote."
     echo "Example: gcrepo my-new-repo"
     return 0
   fi
-  gh repo create "$1" --public --source=. --remote=origin
+  if [[ "$2" == "-p" ]]; then
+    gh repo create "$1" --source=. --public --remote=origin
+    return 0
+  fi
+  gh repo create "$1" --source=. --private --remote=origin
 }
 
-function grpo() {
+function gho() {
   if [[ -z "$1" ]]; then
     echo "Error: repository name is required."
     echo "Use '-h' for help."
     return 1
   fi
   if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "Usage: grpg <repository name>"
+    echo "Usage: gho <repository name>"
     echo "Opens the repo in the browser"
     echo "Example: grpg repo-name" 
     return 0
@@ -30,7 +40,7 @@ function grpo() {
   gh repo view $1 -w
 }
 
-function grpsc() {
+function ghsc() {
   if [[ -z "$1" ]]; then
     echo "Error: repository name is required."
     echo "Use '-h' for help."
@@ -45,14 +55,14 @@ function grpsc() {
   gh repo list | grep -E $1  | awk '{print $1}'
 }
 
-function grprm() {
+function ghrm() {
   if [[ -z "$1" ]]; then
     echo "Error: repository name is required."
     echo "Use '-h' for help."
     return 1
   fi
   if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "Usage: grprm <repository name>"
+    echo "Usage: ghrm <repository name>"
     echo "Delete a GitHub repository" 
     echo "Example: grprm my-repo"
     return 0
@@ -60,14 +70,14 @@ function grprm() {
   gh repo delete "$1" --yes
 }
 
-function grpsc() {
+function ghsc() {
   if [[ -z "$1" ]]; then
     echo "Error: repository name is required."
     echo "Use '-h' for help."
     return 1
   fi
   if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    echo "Usage: grpsc <repository name>"
+    echo "Usage: ghsc <repository name>"
     echo "greps and prints the name of the grepped repositories"
     echo "Example: grpsc carlino"
     return 0
