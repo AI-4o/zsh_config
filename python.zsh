b7fc70cd46416919bbf50f7444c50c9dc25f95ff
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-06-08 21:46:29 +0200

    edit the python aliases

--- python.zsh
+++ python.zsh
@@ -1,15 +1,15 @@
 #pip aliases
 
-alias ppi='pip install'
-alias ppu='pip uninstall'
-alias ppf='pip freeze > requirements.txt'
-alias ppir='pip install -r requirements.txt'
-alias ppl='pip list'
 
 # python3
 
-alias vvi='python3 -m venv .venv'
+alias vvini='python3 -m venv .venv' 
 alias vva=". .venv/bin/activate" 
+alias vvi="pip install"
+alias vvf='pip freeze > requirements.txt'
+alias vvir='pip install -r requirements.txt'
+alias vvl='pip list'
+
 
 alias pn='python3'
 alias pnc="python3 cli.py"
